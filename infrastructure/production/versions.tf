2beb9dd53a3cb606bb88d5d6d254a41c3c2ac376
ihommani <ismaelhommani@gmail.com>
2024-05-19 12:42:31 +0000

    Udpate tf required version

--- infrastructure/production/versions.tf
+++ infrastructure/production/versions.tf
@@ -1,10 +1,10 @@
 terraform {
 
-  required_version = ">= 1.5.7, < 1.6.0"
-  
+  required_version = ">= 1.5.7, < 1.8.0"
+
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = ">= 5.15.0, < 6.0.0"
     }
   }
