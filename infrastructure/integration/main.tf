2dad13bfaf664412227f69200fb37dd68b8c39b2
ihommani <ismaelhommani@gmail.com>
2024-07-18 16:20:52 +0000

    testing cloud run

--- infrastructure/integration/main.tf
+++ infrastructure/integration/main.tf
@@ -7,3 +7,22 @@ resource "null_resource" "default" {
     command = "echo 'Hello integration, how are you'"
   }
 }
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "europe-west1"
+  project  = "gde-ihommani"
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/gde-ihommani/to-delete/frontend:latest"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
